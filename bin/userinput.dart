a4c719aeada07908e7504bfa83289747c623d69a
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-16 14:47:33 +0530

    new

---
+++ bin/userinput.dart
@@ -0,0 +1,12 @@
+import 'dart:io'; //import is used for access the predefined or the builtin data or functions
+
+void main() {
+  print('Enter your name: ');
+
+  ///variable name is the name given to the memory location where we store the value
+  //String name ="AKHI";
+
+  String? name = stdin.readLineSync();
+
+  ///? - null aware
+}
