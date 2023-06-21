1d0224c8ab30d430b89949f04955c9cf0ffbd49c
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-21 13:52:31 +0530

    commit

--- bin/operators.dart
+++ bin/operators.dart
@@ -20,4 +20,13 @@ void main() {
   print('x/=y ->  ${x /= y}'); // x = x/y -> x = 900 /30 = 30
   // x/=y will show error since x and y are integer so div result double can't be assigned to integer x
   // so declare x and y as dynamic
+
+  ///2.UNARY OPERATOR  = postfix, prefix
+
+  int i = 10;
+  print('i = ${i++}'); // 10 bck -> i+i = 10+1 = 11  postfix increment
+  print('i = $i'); // 11
+  print('i = ${i--}'); // 11 bck -> i-i = 11-1 = 10  postfix decrement
+  print('i = ${++i}');
+  print('i = ${--i}');
 }
