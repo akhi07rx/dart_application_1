ced3360735fc18c13dee5899f7437e5a4b8ac318
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-21 14:06:54 +0530

    commit

--- bin/operators.dart
+++ bin/operators.dart
@@ -10,6 +10,8 @@ void main() {
   print('a%b  = ${a % b}'); // to fetch remainder
   print('a~/b = ${a ~/ b}'); // to fetch integer result if the result if double
 
+  print("");
+
   ///2.ASSIGNMENT OPERATOR  = ,  +=, -= ,*=  ..........etc
 
   dynamic x = 20, y = 30;
@@ -21,12 +23,28 @@ void main() {
   // x/=y will show error since x and y are integer so div result double can't be assigned to integer x
   // so declare x and y as dynamic
 
-  ///2.UNARY OPERATOR  = postfix, prefix
+  print("");
+
+  ///3.UNARY OPERATOR  = postfix, prefix
 
   int i = 10;
   print('i = ${i++}'); // 10 bck -> i+i = 10+1 = 11  postfix increment
   print('i = $i'); // 11
   print('i = ${i--}'); // 11 bck -> i-i = 11-1 = 10  postfix decrement
+  print('i = $i');
   print('i = ${++i}');
   print('i = ${--i}');
+
+  print("");
+
+  ///4.RELATIONAL OPERATOR  = > < >= == !=
+
+  int j = 10, k = 23;
+
+  print(j > k); //false
+  print(j < k); //true
+  print(j == k);
+  print(j != k);
+  print(j >= k);
+  print(j <= k);
 }
