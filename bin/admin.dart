600f6743f9e97d22b1759d63659883d5c56111ab
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-21 14:56:18 +0530

    commit

--- bin/admin.dart
+++ bin/admin.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  print('Enter Username: ');
+  String? username = stdin.readLineSync();
+
+  print('Enter Password: ');
+  int password = int.parse(stdin.readLineSync()!);
+
+  print(username == "Admin" && password == 123456);
+  var user = username == 'Admin' ? true : "Incorrect Username";
+  var result = password == "123456" ? true : "Incorrect Password";
+
+  print(result);
+  print(user);
+}
