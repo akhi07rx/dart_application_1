f184987fd4a3ae3d14e6a6a1dce40c7902fb191e
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-06-23 13:47:50 +0530

    Initial Commit

---
+++ bin/loops.dart
@@ -0,0 +1,8 @@
+///Loops - for , while , do-while
+// for loop syntax : for(initialization ; condition check ; increment / decrement counter);
+
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
