55f8924125afecde4b7a4ee1528eb4bc76782d01
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-06 14:35:28 +0530

    Initial Commit

---
+++ bin/collections/types_of_for_loops.dart
@@ -0,0 +1,22 @@
+void main() {
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  //1. Normal For Loop
+  for (int index = 0; index < list.length; index++) {
+    print(list[index]);
+  }
+
+  print('\n');
+
+  //2. For - in Loop syntax for(datatype-similar-to-list variable name in listname){}
+  for (int i in list) {
+    print(i);
+  }
+
+  list.forEach((element) {
+    print(element);
+  });
+
+  int value = list.firstWhere((element) => element > 2);
+  print(value);
+}
