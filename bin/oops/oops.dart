4b244515470ac51b5caf21fa126f8c81600d54cf
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-10 14:00:42 +0530

    Initial Commit

---
+++ bin/oops/oops.dart
@@ -0,0 +1,12 @@
+class Parent {
+  String fathername = "Raju";
+}
+
+class Child {
+  String childname = "AKHI";
+}
+
+void main() {
+  Child obj = Child();
+  print('My name is : ${obj.childname} ${obj1.fathername}');
+}
