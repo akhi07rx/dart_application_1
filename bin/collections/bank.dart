4a36945f4c945a491734fbe344b0191c85f51dde
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-12 14:14:47 +0530

    Initial Commit

---
+++ bin/collections/bank.dart
@@ -0,0 +1,22 @@
+import 'dart:mirrors';
+
+class Bank {
+  int? minbalnc;
+  void deposit(int amount) {
+    print("Min deposit amount is: $amount");
+  }
+
+  void withdraw(int amount) {
+    print("Min withdrawal amount is: $amount");
+  }
+
+  void checkbalance() {
+
+  }
+}
+
+  Class
+
+
+
+
